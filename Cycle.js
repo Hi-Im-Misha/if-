f5f277e516e52174ec45a3c271594bab071d2c58
Misha <mishakalashnikov9.6@gmail.com>
2022-11-06 18:56:58 +0300

    %2

--- Cycle.js
+++ Cycle.js
@@ -1,12 +1,5 @@
-let sum = 0;
-
-while (true) {
-
-    let value = +prompt("Введите число", '');
-
-    if (!value) break; // (*)
-
-    sum += value;
-
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        alert( i );
+    }
 }
-alert( 'Сумма: ' + sum );
