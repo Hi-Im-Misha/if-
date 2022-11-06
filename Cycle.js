b61805f915df25172abd0a1e43f450ab6e3e3ce1
Misha <mishakalashnikov9.6@gmail.com>
2022-11-06 20:04:15 +0300

    100

--- Cycle.js
+++ Cycle.js
@@ -1,5 +1,8 @@
-let i = 0;
-while (i < 3) {
-    alert( `number ${i}!` );
-    i++;
+for (;num < 100 && num;) {
+    const num = prompt("ведите число, больше", "100");
+    if (isNaN(num)) {
+        alert("Нужно написать число");
+    } else if (num >= 100 && num){
+        break;
+    }
 }
